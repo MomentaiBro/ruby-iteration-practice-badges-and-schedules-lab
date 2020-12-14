6277b17fa6843d275f3f88a89030d3b9b89230bd
Alexander Sherwin <alx.sherwin@gmail.com>
2020-12-14 14:05:35 -0700

    commit tree 47c99db09298387dac4e831d2300c2cdbc9ab290

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require "pry"
+
+def badge_maker names
+    "Hello, my name is #{names}."
+end
+
+
+def batch_badge_creator names
+
+    names.collect do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms names
+
+    names.map.with_index do |name, room_number|
+    "Hello, #{name}! You'll be assigned to room #{room_number + 1}!"
+    end
+end
+
+def printer names
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |room|
+        puts room
+    end
+
+
+end
+
